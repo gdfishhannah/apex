0b8267b4156d4206cae3b9deb0655b7b2a0e5980
gdfishhannah <gdfishhannah@gmail.com>
2018-10-21 16:51:15 +0800

    update

--- examples/imagenet/deploy.sh
+++ examples/imagenet/deploy.sh
@@ -13,28 +13,35 @@ if [ ! -x "$imagefile" ]; then
         mkdir /home/ImageNet
         mkdir /home/ImageNet/train
         mkdir /home/ImageNet/train/1
+        
+        cd /home/ImageNet/train/1
+        wget http://farm4.static.flickr.com/3579/3338602889_c1c371939f.jpg -O 1.jpg
+        
+        i=2
+        while [ $i -le 1000 ]
+        do
+                cp 1.jpg $i.jpg
+                let i+=1
+        done
+
+        cd /home/ImageNet/train
+        i=2
+        while [ $i -le 10 ]
+        do
+                cp -r /home/ImageNet/train/1 /home/ImageNet/train/$i
+                let i+=1
+        done
+
+        cp -r /home/ImageNet/train /home/ImageNet/val
 fi
 
-cd /home/ImageNet/train/1
-wget http://farm4.static.flickr.com/3579/3338602889_c1c371939f.jpg -O 1.jpg
-
-i=2
-while [ $i -le 1000 ]
-do
-        cp 1.jpg $i.jpg
-        let i+=1
-done
-
-cd /home/ImageNet/train
-i=2
-while [ $i -le 10 ]
-do
-        cp -r /home/ImageNet/train/1 /home/ImageNet/train/$i
-        let i+=1
-done
-
-cp -r /home/ImageNet/train /home/ImageNet/val
-
-
-# download code 
-git clone https://github.com/gdfishhannah/apex.git
+# download code
+if [ ! -x "apex" ]; then
+        git clone https://github.com/gdfishhannah/apex.git
+fi
+cd apex
+python uninstall apex -y
+python setup.py install
+cd examples/imagenet/
+chmod a+x *.sh
+./DDP.sh
